818961f4b6b5c2c3bcfd195bd6a618133b21dd02
Vaibhav Bhadoriya <Vbhadoriya78@gmail.com>
2026-07-09 16:34:02 +0530

    Completed Linear model practice in VS code

--- ML Practice/ML_01_Linear.py
+++ ML Practice/ML_01_Linear.py
@@ -10,6 +10,7 @@ print(df.head())
 
 ## Check for null values
 print(df.isnull().sum())
+df = df.dropna()
 print(df.describe())
 
 ## Scatter plot
@@ -24,3 +25,44 @@ print(df.corr())
 ## Independent and dependent features
 X = df[['x']]
 y = df['y']
+
+## Train test split
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.3,random_state=42)
+## Train Model 
+from sklearn.linear_model import LinearRegression
+reg = LinearRegression()
+reg.fit(X_train,y_train)
+
+## Pridictions 
+pred = reg.predict(X_test)
+print(pred[:5])
+
+"""from sklearn.metrics import mean_squared_error
+mse = mean_squared_error(y_test, pred)
+print("MSE :",mse)
+
+from sklearn.metrics import r2_score 
+r2 = r2_score(y_test, pred)
+print("R2 score", r2)"""
+comparison = pd.DataFrame({
+    "Actual": y_test,
+    "Predicted": pred
+})
+
+print(comparison)
+from sklearn.metrics import mean_squared_error
+mse = mean_squared_error(y_test, pred)
+print("MSE :",mse)
+from sklearn.metrics import r2_score
+r2 = r2_score(y_test, pred)
+print("R2 score", r2)
+"""plt.scatter(y_test, pred)
+plt.xlabel("Actual")
+plt.ylabel("Predicted")
+plt.title("Actual vs Predicted")
+plt.show()"""
+## Plot the Training data best fit line
+plt.scatter(X_train,y_train)
+plt.plot(X_train,reg.predict(X_train))
+plt.show()
